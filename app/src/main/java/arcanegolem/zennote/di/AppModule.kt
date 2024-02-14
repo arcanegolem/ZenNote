422597e42e8ae0024d01aeea2e3b8931fb9b5ebc
arcanegolem <dopeflipper@gmail.com>
2024-02-14 04:18:58 +0300

    DI initial setup

--- app/src/main/java/arcanegolem/zennote/di/AppModule.kt
+++ app/src/main/java/arcanegolem/zennote/di/AppModule.kt
@@ -1,7 +1,9 @@
 package arcanegolem.zennote.di
 
+import arcanegolem.zennote.presentation.screens.main.MainScreenViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-
+   viewModelOf(::MainScreenViewModel)
 }
